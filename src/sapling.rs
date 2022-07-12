1a485a7d839db95ca8901f5e143d6fc7edfebf02
mschoenebeck <matthias.schoenebeck@gmail.com>
2022-07-11 21:28:57 -0500

    bug fixies after rename. works with JS wallet again

--- src/sapling.rs
+++ src/sapling.rs
@@ -425,211 +425,10 @@ pub fn decrypt_serde_object<T: Serialize + DeserializeOwned>(key: &[u8; 32], aes
 // to smart contract json function (VK, Proof, Inputs)
 pub fn to_json<T>(var: &T) -> String
 {
-    // struct definitions
-    pub struct Choice(pub u8);
-    pub struct Fp(pub [u64; 6]);
-    pub struct Scalar(pub [u64; 4]);
-    pub struct Fp2
-    {
-        pub c0: Fp,
-        pub c1: Fp,
-    }
-    pub struct G1Affine
-    {
-        pub x: Fp,
-        pub y: Fp,
-        pub infinity: Choice,
-    }
-    pub struct G2Affine
-    {
-        pub x: Fp2,
-        pub y: Fp2,
-        pub infinity: Choice,
-    }
-
     let mut json = String::new();
 
     match type_of(var)
     {
-        "bellman::groth16::VerifyingKey<bls12_381::pairings::Bls12>" =>
-        {
-            json.push('{');
-
-            let vk: &VerifyingKey<Bls12> = unsafe {&*(var as *const T as *const VerifyingKey<Bls12>)};
-               
-            json.push_str("\"alpha_g1\":");
-            json.push_str(to_json(&vk.alpha_g1).as_str());
-            json.push(',');
-               
-            json.push_str("\"beta_g1\":");
-            json.push_str(to_json(&vk.beta_g1).as_str());
-            json.push(',');
-               
-            json.push_str("\"beta_g2\":");
-            json.push_str(to_json(&vk.beta_g2).as_str());
-            json.push(',');
-               
-            json.push_str("\"gamma_g2\":");
-            json.push_str(to_json(&vk.gamma_g2).as_str());
-            json.push(',');
-               
-            json.push_str("\"delta_g1\":");
-            json.push_str(to_json(&vk.delta_g1).as_str());
-            json.push(',');
-               
-            json.push_str("\"delta_g2\":");
-            json.push_str(to_json(&vk.delta_g2).as_str());
-            json.push(',');
-               
-            json.push_str("\"ic\":");
-            json.push_str(to_json(&vk.ic).as_str());
-            
-            json.push('}');
-        },
-        "bellman::groth16::Proof<bls12_381::pairings::Bls12>" =>
-        {
-            json.push('{');
-
-            let proof: &Proof<Bls12> = unsafe {&*(var as *const T as *const Proof<Bls12>)};
-               
-            json.push_str("\"a\":");
-            json.push_str(to_json(&proof.a).as_str());
-            json.push(',');
-               
-            json.push_str("\"b\":");
-            json.push_str(to_json(&proof.b).as_str());
-            json.push(',');
-               
-            json.push_str("\"c\":");
-            json.push_str(to_json(&proof.c).as_str());
-            
-            json.push('}');
-        },
-        "rustzeos::to_json::G1Affine" |
-        "bls12_381::g1::G1Affine" =>
-        {
-            json.push('{');
-
-            let g1: &G1Affine = unsafe {&*(var as *const T as *const G1Affine)};
-               
-            json.push_str("\"x\":");
-            json.push_str(to_json(&g1.x).as_str());
-            json.push(',');
-               
-            json.push_str("\"y\":");
-            json.push_str(to_json(&g1.y).as_str());
-            json.push(',');
-               
-            json.push_str("\"infinity\":");
-            json.push_str(to_json(&g1.infinity).as_str());
-            
-            json.push('}');
-        },
-        "rustzeos::to_json::G2Affine" |
-        "bls12_381::g2::G2Affine" =>
-        {
-            json.push('{');
-
-            let g2: &G2Affine = unsafe {&*(var as *const T as *const G2Affine)};
-               
-            json.push_str("\"x\":");
-            json.push_str(to_json(&g2.x).as_str());
-            json.push(',');
-               
-            json.push_str("\"y\":");
-            json.push_str(to_json(&g2.y).as_str());
-            json.push(',');
-               
-            json.push_str("\"infinity\":");
-            json.push_str(to_json(&g2.infinity).as_str());
-            
-            json.push('}');
-        },
-        "alloc::vec::Vec<bls12_381::g1::G1Affine>" =>
-        {
-            json.push('[');
-
-            let vec: &Vec<G1Affine> = unsafe {&*(var as *const T as *const Vec<G1Affine>)};
-
-            for v in vec
-            {
-                json.push_str(to_json(v).as_str());
-                json.push(',');
-            }
-            json.pop();
-
-            json.push(']');
-        },
-        "alloc::vec::Vec<bls12_381::scalar::Scalar>" =>
-        {
-            json.push('[');
-
-            let vec: &Vec<Scalar> = unsafe {&*(var as *const T as *const Vec<Scalar>)};
-
-            for v in vec
-            {
-                json.push_str(to_json(v).as_str());
-                json.push(',');
-            }
-            json.pop();
-
-            json.push(']');
-        },
-        "rustzeos::to_json::Fp" => 
-        {
-            json.push_str("{\"data\":[");
-            
-            let fp: &Fp = unsafe {&*(var as *const T as *const Fp)};
-
-            for v in fp.0
-            {
-                json.push_str(format!("{}", v).as_str());
-                json.push(',');
-            }
-            json.pop();
-
-            json.push_str("]}");
-        },
-        "rustzeos::to_json::Scalar" => 
-        {
-            json.push_str("{\"data\":[");
-            
-            let scalar: &Scalar = unsafe {&*(var as *const T as *const Scalar)};
-
-            for v in scalar.0
-            {
-                json.push_str(format!("{}", v).as_str());
-                json.push(',');
-            }
-            json.pop();
-
-            json.push_str("]}");
-        },
-        "rustzeos::to_json::Fp2" => 
-        {
-            json.push('{');
-
-            let fp2: &Fp2 = unsafe {&*(var as *const T as *const Fp2)};
-               
-            json.push_str("\"c0\":");
-            json.push_str(to_json(&fp2.c0).as_str());
-            json.push(',');
-               
-            json.push_str("\"c1\":");
-            json.push_str(to_json(&fp2.c1).as_str());
-            
-            json.push('}');
-        },
-        "rustzeos::to_json::Choice" =>
-        {
-            json.push_str("{\"data\":");
-            
-            let c: &Choice = unsafe {&*(var as *const T as *const Choice)};
-            
-            json.push_str(format!("{}", c.0).as_str());
-            
-            json.push('}');
-        },
         "blake2s_simd::Hash" =>
         {
             let h: &Hash = unsafe {&*(var as *const T as *const Hash)};
@@ -639,42 +438,42 @@ pub fn to_json<T>(var: &T) -> String
                 json.push_str(format!("{:02x}", byte).as_str());
             }
         },
-        "rustzeos::Asset" =>
+        "sapling::Asset" =>
         {
             let a: &Asset = unsafe {&*(var as *const T as *const Asset)};
             json.push_str(&serde_json::to_string(a).unwrap());
         },
-        "rustzeos::Transaction" =>
+        "sapling::Transaction" =>
         {
             let tx: &Transaction = unsafe {&*(var as *const T as *const Transaction)};
             json.push_str(&serde_json::to_string(tx).unwrap());
         },
-        "rustzeos::EncryptedTransaction" =>
+        "sapling::EncryptedTransaction" =>
         {
             let enc_tx: &EncryptedTransaction = unsafe {&*(var as *const T as *const EncryptedTransaction)};
             json.push_str(&serde_json::to_string(enc_tx).unwrap());
         },
-        "rustzeos::TxSender" =>
+        "sapling::TxSender" =>
         {
             let txs: &TxSender = unsafe {&*(var as *const T as *const TxSender)};
             json.push_str(&serde_json::to_string(txs).unwrap());
         },
-        "rustzeos::TxReceiver" =>
+        "sapling::TxReceiver" =>
         {
             let txr: &TxReceiver = unsafe {&*(var as *const T as *const TxReceiver)};
             json.push_str(&serde_json::to_string(txr).unwrap());
         },
-        "rustzeos::Note" =>
+        "sapling::Note" =>
         {
             let n: &Note = unsafe {&*(var as *const T as *const Note)};
             json.push_str(&serde_json::to_string(n).unwrap());
         },
-        "rustzeos::Symbol" =>
+        "sapling::Symbol" =>
         {
             let s: &Symbol = unsafe {&*(var as *const T as *const Symbol)};
             json.push_str(&serde_json::to_string(s).unwrap());
         },/*
-        "core::option::Option<rustzeos::TxSender>" =>
+        "core::option::Option<sapling::TxSender>" =>
         {
             let op_txs: &Option<TxSender> = unsafe {&*(var as *const T as *const Option<TxSender>)};
             let txs = match op_txs {
@@ -683,7 +482,7 @@ pub fn to_json<T>(var: &T) -> String
             };
             json.push_str(&txs);
         },
-        "core::option::Option<rustzeos::TxReceiver>" =>
+        "core::option::Option<sapling::TxReceiver>" =>
         {
             let op_txr: &Option<TxReceiver> = unsafe {&*(var as *const T as *const Option<TxReceiver>)};
             let txr = match op_txr {
@@ -692,12 +491,12 @@ pub fn to_json<T>(var: &T) -> String
             };
             json.push_str(&txr);
         },*/
-        "rustzeos::SecretKey" =>
+        "sapling::SecretKey" =>
         {
             let sk: &[u8; 32] = unsafe {&*(var as *const T as *const [u8; 32])};
             json.push_str(&serde_json::to_string(sk).unwrap());
         },
-        "rustzeos::Address" =>
+        "sapling::Address" =>
         {
             let addr: &Address = unsafe {&*(var as *const T as *const Address)};
             json.push_str(&serde_json::to_string(addr).unwrap());
